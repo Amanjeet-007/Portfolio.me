18a35778f0bc31f6e87f0c269a7151afca03857f
Amanjeet Kumar <kumaramanjeet951@gmail.com>
2025-03-05 16:46:40 +0530

    menu

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -12,8 +12,8 @@ export default function Navbar() {
                     <path fillRule="evenodd" d="M3 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 5.25Zm0 4.5A.75.75 0 0 1 3.75 9h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 9.75Zm0 4.5a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Zm0 4.5a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd"  />
                 </svg>
                 </div>
-               
-                <div className="menu h-min w-min bg-white/10 backdrop-blur-md border border-white/20 shadow-lg rounded-lg p-6 absolute top-26 left-[33%] flex items-center " style={{visibility:(show?"visible":"hidden"),scale:(show?"1":"0")}} >
+               <div className="w-full flex justify-center absolute top-27 left-0">
+                 <div className="menu h-min w-min bg-white/10 backdrop-blur-md border border-white/20 shadow-lg rounded-lg p-4 flex items-center " style={{visibility:(show?"visible":"hidden"),scale:(show?"1":"0")}} >
                 <ul className="text-white flex ">
                     <li className="flex justify-center mx-2 my-1 py-1 px-8 tracking-widest cursor-pointer rounded-2xl">Home</li>
                     <li className="flex justify-center mx-2 my-1 py-1 px-8 tracking-widest cursor-pointer rounded-2xl">Projects</li>
@@ -21,6 +21,8 @@ export default function Navbar() {
                     <li className="flex justify-center mx-2 my-1 py-1 px-8 tracking-widest cursor-pointer rounded-2xl">About</li>
                 </ul>
                 </div>
+               </div>
+               
                 <div className="name mx-10 text-4xl align-middle">Amanjeet</div>
                 
             </nav>
