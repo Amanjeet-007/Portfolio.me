746e5b752a4ec09b4d6dc3882a07507cf3f4facb
Amanjeet Kumar <kumaramanjeet951@gmail.com>
2025-10-16 10:04:34 +0530

    design change

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,74 +1,74 @@
-import { motion ,AnimatePresence } from 'motion/react'
-import { useState } from 'react'
-import { NavLink } from 'react-router'
+import { useEffect, useRef } from 'react';
+
+import '../styles/Nav.css'
+import gsap from 'gsap';
+import Options from './Options';
+
 export default function Navbar() {
-    const [isClick,setIsClick] = useState(false);
-    const options = ["Home","Projects","Blogs","Connect"] //"About",
+    useEffect((e) => {
+        const Old = document.querySelectorAll(".neo")
+        const New = document.querySelectorAll(".new")
+        const logo = document.querySelector("#logo")
+        const tl = gsap.timeline()
+        gsap.set(Old, {
+            y: 20
+        })
+        // logo Enter
+        const handleEnter = () => {
+            tl.to(Old, {
+                y: -5,
+                opacity: 0,
+                stagger: 0.09,
+            })
+            tl.to(New, {
+                y: -25,
+                opacity: 1,
+                stagger: 0.09,
+            })
+        }
+        // logo Exit
+        const handleExist = () => {
+            tl.to(New, {
+                y: 5,
+                opacity: 0,
+                stagger: 0.09,
+            })
+            tl.to(Old, {
+                y: 25,
+                opacity: 1,
+                stagger: 0.09,
+            })
+        }
+
+
+        logo.addEventListener("mouseenter", handleEnter)
+        logo.addEventListener("mouseleave", handleExist)
+        // cleanup
+        return () => {
+            logo.removeEventListener("mouseenter", handleEnter);
+            logo.removeEventListener("mouseleave", handleExist)
+        };
+    }, [])
 
     return (
-        <>
-        <nav className="w-full h-min flex items-center justify-between">
-           
-
-            <pre className='logo'>{'< Code />'}</pre>
-            <div className="menu md:none w-min h-full">
-                <ul className='flex'>
-                    {options.map(el=>{
-                        return(
-                            <NavLink to={`/${el.toLowerCase()}`} key={el}>  <motion.li
-                            whileHover={{scaleX:1.1,scaleY:1.2,scaleZ:1.1,color:'white'}}
-                            >{el}</motion.li> </NavLink>
-                        )
-                    })}
-                </ul>
-               
+        <nav>
+            <div id="logo">
+                <p>
+                    {/* {["Neo"].map(el=>{return( <span className='neo'>{el}</span>)})} */}
+                    <span className='neo'>N</span>
+                    <span className='neo'>e</span>
+                    <span className='neo'>o</span>
+                </p>
+                <p>
+                    <span className='new'>N</span>
+                    <span className='new'>e</span>
+                    <span className='new'>w</span>
+                </p>
             </div>
-            <AnimatePresence>
-            <motion.div className="menu-button"
-            onClick={()=>setIsClick(el=>!el)}
-            animate={isClick?{rotate:360}:{rotate:0}}
-            whileHover={{scale:1.1}}
-            >
-                <div className="curcle">
-                    <motion.span className='bg-emerald-600  w-20 h-20' transition={{duration:.1}} animate={isClick?{opacity:1,scale:1}:{opacity:0,scale:0}}>
-                        <button className="theme">Chnage</button>
-                    </motion.span>
-                </div>
-                <motion.div className="MobileList absolute top-[70px] -right-3"
-                initial={{opacity:0}}
-                animate={isClick?{opacity:1,scale:1}:{opacity:0,scale:0}}
-                >
-                    <NavLink to={"/home"}>
-                    <div className="icons">
-                        <img src="https://cdn-icons-png.flaticon.com/128/10349/10349274.png" alt="home" />
-                    </div>
-                        </NavLink>
-                    <NavLink to={"/projects"}>
-                    <div className="icons"><img src="https://cdn-icons-png.flaticon.com/128/16291/16291033.png" alt="project" /></div>
-                    </NavLink>
-                    <NavLink to={"/blogs"}>
-                    <div className="icons"><img src="https://cdn-icons-png.flaticon.com/128/3959/3959425.png" alt="Blog" /></div>
-                    </NavLink>
-                    <NavLink to={'/connect'}>
-                    <div className="icons"><img src="https://cdn-icons-png.flaticon.com/128/9333/9333996.png" alt="Connect" /></div>
-                    </NavLink>
-                    {/* <NavLink to={'/about'}>
-                    <div className="icons"><img src="https://cdn-icons-png.flaticon.com/128/1/1176.png" alt="About" /></div>
-                    </NavLink> */}
 
-                </motion.div>
-                <motion.div className="box"  animate={isClick?{scale:0,opacity:0}:{scale:1,opacity:1}} ></motion.div>
-                <motion.div className="box" animate={isClick?{scale:0,opacity:0}:{backgroundColor:"white"}}></motion.div>
-                <motion.div className="box" animate={isClick?{scale:0,opacity:0}:{scale:1,opacity:1}}></motion.div>
-                <motion.div className="box flex items-center justify-center text-white text-xl font-bold" animate={isClick?{backgroundColor:"#DA0037",height:"25px",width:"25px"}:{backgroundColor:"white"}} whileHover={isClick?{scale:1.1}:""}>
-                   {isClick?"X":""}
-                </motion.div>
-            </motion.div>
-            </AnimatePresence>
+            <Options/>
+            
+            <button className='cursor-pointer'>Explore</button>
         </nav>
-         {/* <div className="option absolute top-[80px] bg-red-600 h-min w-min right-20">
-         hiui
-             </div> */}
-            </>
     )
 }
---
+++ src/component/Options.jsx
@@ -0,0 +1,112 @@
+import { useEffect } from "react"
+import { NavLink } from "react-router";
+import '../styles/Nav.css'
+import gsap from 'gsap';
+
+export default function Options(){
+    const options = [
+        ["Home", "https://cdn-icons-png.flaticon.com/128/10307/10307931.png", "/home"],
+        ["Projects", "https://cdn-icons-png.flaticon.com/128/9672/9672290.png", "/projects"],
+        ["About", "https://cdn-icons-png.flaticon.com/128/13984/13984272.png", '/about'],
+        ["Blogs", "https://cdn-icons-png.flaticon.com/128/4136/4136043.png", '/blogs'],
+        ["Connect", "https://cdn-icons-png.flaticon.com/128/10308/10308279.png", '/connect']
+    ]
+
+    function hoverHandler(e) {
+        const paragraph = e.currentTarget.querySelector("p");
+        const icon = e.currentTarget.querySelector("span img")
+        gsap.to(e.currentTarget, {
+            scale: 1.2,
+        })
+        gsap.to(paragraph, {
+            rotationY: 45,
+            opacity: 0,
+        })
+        gsap.to(icon, {
+            rotationY: -180,
+            opacity: 1
+        })
+    }
+
+    function exitHandler(e) {
+        const paragraph = e.currentTarget.querySelector("p");
+        const icon = e.currentTarget.querySelector("span img")
+        gsap.to(e.currentTarget, {
+            scale: 1,
+            duration: .5
+        }
+        )
+        gsap.to(paragraph, {
+            rotationY: 0,
+            opacity: 1,
+        })
+        gsap.to(icon, {
+            rotationY: -45,
+            opacity: 0
+        })
+    }
+    useEffect((e) => {
+        const Old = document.querySelectorAll(".neo")
+        const New = document.querySelectorAll(".new")
+        const logo = document.querySelector("#logo")
+        const tl = gsap.timeline()
+        gsap.set(Old, {
+            y: 20
+        })
+        // logo Enter
+        const handleEnter = () => {
+            tl.to(Old, {
+                y: -5,
+                opacity: 0,
+                stagger: 0.09,
+            })
+            tl.to(New, {
+                y: -25,
+                opacity: 1,
+                stagger: 0.09,
+            })
+        }
+        // logo Exit
+        const handleExist = () => {
+            tl.to(New, {
+                y: 5,
+                opacity: 0,
+                stagger: 0.09,
+            })
+            tl.to(Old, {
+                y: 25,
+                opacity: 1,
+                stagger: 0.09,
+            })
+        }
+
+
+        logo.addEventListener("mouseenter", handleEnter)
+        logo.addEventListener("mouseleave", handleExist)
+        // cleanup
+        return () => {
+            logo.removeEventListener("mouseenter", handleEnter);
+            logo.removeEventListener("mouseleave", handleExist)
+        };
+    }, [])
+    return(
+        <>
+    <div id="options">
+                {options.map((el, i) => {
+                    return (
+                        <NavLink to={el[2]} key={i}>
+                            <div onMouseEnter={hoverHandler}
+                                onMouseLeave={exitHandler}
+                                className="option" >
+                                <p>{el[0]}</p>
+                                <span>
+                                    <img src={el[1]} alt="" />
+                                </span>
+                            </div>
+                        </NavLink>
+                    )
+                })}
+            </div>
+        </>
+    )
+}
